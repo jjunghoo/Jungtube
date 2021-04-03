3d4c0a6297fb04a48902c30849624aaf61e999ec
jjunghoo <wjdgn2468@naver.com>
2021-04-03 11:32:04 +0900

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=KR&key=AIzaSyBTsIFd-qUrnA9-aEozLLvXSeTs550KsTs", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />
+}
+
+export default App;
