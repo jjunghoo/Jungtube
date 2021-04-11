07812c45f7c0ae5647c759a08192e932a0d975ad
jjunghoo <wjdgn2468@naver.com>
2021-04-12 04:58:47 +0900

    add replies

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -25,7 +25,6 @@ const SearchHeader = memo(({onSearch}) => {
     return (
         <header className={styles.header}>
             <div className={styles.logo} onClick={home}>
-                <img className={styles.img} src="/images/logo.png" alt="logo"/>
                 <h1 className={styles.title}>Jungtube</h1>
             </div>
             <input
--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './video_detail.module.css';
 
-const VideoDetail = (props) => (
+const VideoDetail = (props) => {
+    const [showReply, setShowReply] = useState(false);
+
+    const onReply = () => {
+        setShowReply(!showReply);
+    };
+
+    const styleType = showReply === false ? styles.displayNone : styles.displayBlock;
+    return(
     <section className={styles.detail}>
         <iframe
             className={styles.video}  
@@ -13,7 +21,7 @@ const VideoDetail = (props) => (
             frameBorder="0" 
             allowFullScreen>
         </iframe>
-        <h2>{props.video.snippet.title}</h2>
+        <h2 className={styles.videoTitle}>{props.video.snippet.title}</h2>
         <div className={styles.channelTitle}>
             {props.channels.map(channel => (
                 <img className={styles.channelImg} key={channel.id} src={channel.snippet.thumbnails.default.url} alt="channelImg" />
@@ -21,7 +29,7 @@ const VideoDetail = (props) => (
             <div className={styles.channelInfo}>
                 <p>{props.video.snippet.channelTitle}</p>
                 {props.channels.map(channel => (
-                    <p className={styles.subscriberCount} key={channel.id}>{channel.statistics.subscriberCount}</p>
+                    <p className={styles.subscriberCount} key={channel.id}>구독자 {channel.statistics.subscriberCount}명</p>
                 ))}
             </div>
         </div>
@@ -34,15 +42,29 @@ const VideoDetail = (props) => (
                 <div className={styles.reply} key={comment.etag}>
                     <img className={styles.authorProfileImg} src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="authorProfileImageUrl"/>
                     <div className={styles.reply__content}>
+                        <p className={styles.authorDisplayName} key={comment.snippet.topLevelComment.snippet.authorChannelId.value}>{comment.snippet.topLevelComment.snippet.authorDisplayName}</p>
                         <p className={styles.textOriginal}>{comment.snippet.topLevelComment.snippet.textOriginal}</p>
                         <i className="fas fa-thumbs-up"></i>
-                        <span>{comment.snippet.topLevelComment.snippet.likeCount}</span>
-
+                        <span className={styles.likeCount}>{comment.snippet.topLevelComment.snippet.likeCount}</span>
+                        <p className={styles.showReplies} onClick={onReply}>댓글 더보기</p>
+                        <div>
+                            {comment.replies && comment.replies.comments.map(reply => ( 
+                                (comment.id === reply.snippet.parentId ? (<div className={`${styleType}`} key={reply.id}>
+                                    <img className={styles.replies__authorProfileImg} src={reply.snippet.authorProfileImageUrl} alt="authorProfileImageUrl"/>
+                                    <div className={styles.replies__content}>
+                                        <p className={styles.replies_authorDisplayName}>{reply.snippet.authorDisplayName}</p>
+                                        <p className={styles.replies_textOriginal}>{reply.snippet.textOriginal}</p>
+                                        <i className="fas fa-thumbs-up"></i>
+                                        <span className={styles.likeCount}>{reply.snippet.likeCount}</span>
+                                    </div>
+                                </div>) : null)
+                            ))}
+                        </div>
                     </div>
                 </div>
             ))}
         </div>
     </section>
-);
+)};
 
 export default VideoDetail;
